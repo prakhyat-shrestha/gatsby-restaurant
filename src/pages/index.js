0faa31a607f08e207df5e220d56f594a40ac505c
prakhyat <erprakhyat@gmail.com>
2019-02-10 12:56:44 +0545

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { HomeHeader, Banner, BannerButton } from "../utils"
+import img from "../images/bcg/homeBcg.jpeg"
+import QuickInfo from "../components/HomePageComponents/QuickInfo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    hello brocky
+    <HomeHeader img={img}>
+      <Banner title="eatery" subtitle="55 main street- Santa Monica, CA">
+        <Link to="/menu/" style={{ textDecoration: "none" }}>
+          <BannerButton style={{ margin: "2rem auto" }}>menu</BannerButton>
+        </Link>
+      </Banner>
+    </HomeHeader>
+    <QuickInfo />
   </Layout>
 )
 
